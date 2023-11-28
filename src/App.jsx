0860bc218d469cf716f03725c5fbd25a5edf6488
Jostein Dyrseth <josdyr@gmail.com>
2023-11-28 12:18:19 +0100

    Add Table component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+function Table({ value }) {
+  return <table className="table">Hi Table</table>;
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,6 +15,9 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <p>Hello World!</p>
+      </div>
+      <div className="table">
+        <Table />
       </div>
     </>
   );
