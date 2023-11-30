e5dcf5e6754d869b56a84d86e63e8d74dbfa5f1b
Jostein Dyrseth <josdyr@gmail.com>
2023-11-30 13:15:53 +0100

    Update main.jsx to use Root component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
