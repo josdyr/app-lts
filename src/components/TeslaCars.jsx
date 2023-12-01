d9b297a76e8ed38761819144a5f0cbb496ac7fa1
Jostein Dyrseth <josdyr@gmail.com>
2023-12-01 09:39:43 +0100

    Add TeslaCars Outlet component

--- src/components/TeslaCars.jsx
+++ src/components/TeslaCars.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
 const TeslaCars = () => {
   const [teslaCars, setTeslaCars] = useState([]);
@@ -52,6 +53,7 @@ const TeslaCars = () => {
         </thead>
         <tbody>{renderTable()}</tbody>
       </table>
+      <Outlet />
     </div>
   );
 };
