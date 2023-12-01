d977a4d04a7f295eddb96a777ba06f97dd1686db
Jostein Dyrseth <josdyr@gmail.com>
2023-12-01 09:43:03 +0100

    Refactor routing in App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import TeslaCars from "./TeslaCars";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import TeslaCars from "./components/TeslaCars";
+import { Navbar } from "./components/Navbar";
+import { NoMatch } from "./components/NoMatch";
+import { ObjectDetail } from "./components/ObjectDetail";
+import { Users } from "./components/Users";
+import { UserDetails } from "./components/UserDetails";
+import { Admin } from "./components/Admin";
+import { Create } from "./components/Create";
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/tesla-cars" element={<TeslaCars />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="tesla-cars" element={<TeslaCars />}>
+          <Route path=":id" element={<ObjectDetail />} />
+        </Route>
+        <Route path="create" element={<Create />} />
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </>
   );
 };
 
