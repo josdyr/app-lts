d70b358adc115dd6bfffbd56c71da17d22eb80fd
Jostein Dyrseth <josdyr@gmail.com>
2023-12-16 15:39:05 +0100

    Add WebSocket subscription functionality

---
+++ src/subscribe.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws');
+const { WebPubSubServiceClient } = require('@azure/web-pubsub');
+
+async function main() {
+    const hub = "pubsub";
+    let service = new WebPubSubServiceClient(process.env.WebPubSubConnectionString, hub);
+    let token = await service.getClientAccessToken();
+    let ws = new WebSocket(token.url);
+    ws.on('open', () => console.log('connected'));
+    ws.on('message', data => console.log('Message received: %s', data));
+}
+
+main();
